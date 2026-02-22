c014ad98c732e9fa59cebbc06d17419905e84ec2
Jeison-Rosario <jsrosario97@gmail.com>
2026-02-22 18:06:24 -0400

    update exchange URL to use the latest moneyconvert API

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # CONFIG
 # =========================
 
-EXCHANGE_URL = "https://api.exchangerate-api.com/v4/latest/USD"
+EXCHANGE_URL = "https://cdn.moneyconvert.net/api/latest.json"
 
 BOT_TOKEN = "8279723703:AAGpbxC7gRPbUrWEV7WMMC5GmVOL2eV5NOA"  # 🔐 Variable de entorno
 CHAT_IDS = ["1180916427", "6158759375"]
